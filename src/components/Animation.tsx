a337dd603e9474582962db6d3103b8566589fd62
ledamint <heresliker@gmail.com>
2020-07-26 18:41:50 +0300

    pass filepath to animation

--- src/components/Animation.tsx
+++ src/components/Animation.tsx
@@ -1,10 +1,7 @@
-import fs from 'fs';
 import React from 'react';
 import { Animation } from '@urban-bot/core';
 import filepath from '../assets/animation.gif';
 
-const file = fs.createReadStream(filepath);
-
 export function AnimationExample() {
-    return <Animation file={file} title="There is the animation" name="File Name" />;
+    return <Animation file={filepath} title="There is the animation" name="File Name" />;
 }
