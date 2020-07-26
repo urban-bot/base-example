1efb6c9955e3d08bc4e1893e6498991f74a95fdf
ledamint <heresliker@gmail.com>
2020-07-26 19:46:39 +0300

    change stream to readFileSync

--- src/components/File.tsx
+++ src/components/File.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { File } from '@urban-bot/core';
 import filepath from '../assets/README.md';
 
-const file = fs.createReadStream(filepath);
+const file = fs.readFileSync(filepath);
 
 export function FileExample() {
     return <File file={file} title="There is the file" />;
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Video } from '@urban-bot/core';
 import filepath from '../assets/video.mp4'
 
-const file = fs.createReadStream(filepath);
+const file = fs.readFileSync(filepath);
 
 export function VideoExample() {
     return <Video file={file} title="There is the video" />;
